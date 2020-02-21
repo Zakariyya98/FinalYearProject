7c117c18d7b8a0836f19ae98b3cb623a4e6a1468
Zakariyya Ahmed <ahmedz27@coventry.ac.uk>
2020-02-21 15:39:14 +0000

    Applied Dataset and Prediction

--- Test/TensorFlowImageRecognition/imagerecognition.py
+++ Test/TensorFlowImageRecognition/imagerecognition.py
@@ -1,13 +1,42 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#Keras doesnt wanna work yet ????
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+#from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 import os
 import numpy as np
 import matplotlib as plt
+
+#Dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10)
+])
+
+predictions = model(x_train[:1]).numpy()
+predictions
+
+tf.nn.softmax(predictions).numpy()
+
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer = 'adam',
+              loss = loss_fn,
+              metrics=['accuracy'])
+              
 
 
 
