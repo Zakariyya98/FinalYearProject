125d0288413d5c3cf4ddec6d58bbdc2706cd5790
Zakariyya Ahmed <ahmedz27@coventry.ac.uk>
2020-03-16 19:17:00 +0000

    gitignore file added

--- Test/TensorFlowImageRecognition/imagerecognition.py
+++ Test/TensorFlowImageRecognition/imagerecognition.py
@@ -1,11 +1,16 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf 
-
+import keras
 #Keras doesnt wanna work yet ????
-#from tensorflow.keras.models import Sequential
-#from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-#from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow.keras.models 
+
+#import Sequential
+import tensorflow.keras.layers 
+#import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+import tensorflow.keras.preprocessing.image 
+
+#import ImageDataGenerator
 
 import os
 import numpy as np
