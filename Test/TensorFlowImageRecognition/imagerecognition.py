56223638904b5d72112580498a7e13778811bc6d
Zakariyya Ahmed <ahmedz27@coventry.ac.uk>
2020-03-19 18:59:25 +0000

    New Commit

--- Test/TensorFlowImageRecognition/imagerecognition.py
+++ Test/TensorFlowImageRecognition/imagerecognition.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-
 import tensorflow as tf 
 from tensorflow import keras
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,11 +13,8 @@ class_names =['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', '
 train_images.shape
 
 len(train_labels)
-
 train_labels
-
 test_image.shape
-
 len(test_labels)
 
 plt.figure()
@@ -29,7 +24,6 @@ plt.grid(False)
 plt.show()
 
 train_images = train_images / 255.0
-
 test_image = test_image / 255.0
 
 plt.figure(figsize=(10,10))
@@ -63,9 +57,7 @@ probability_model = tf.keras.Sequential([model,
                                         tf.keras.layers.Softmax()])
 
 predictions = probability_model.predict(test_image)
-
 predictions[0]
-
 np.argmax(predictions[0])
 
 test_labels[0]
@@ -89,17 +81,17 @@ def plot_image(i, predictions_array, train_label, img):
                                class_names[true_label]),
                                color=color)
 
-    def plot_value_array(i, predictions_array, true_label):
-        predictions_array, true_label = predictions_array, true_label[i]
-        plt.grid(False)
-        plt.xticks(range(10))
-        plt.yticks([])
-        thisplot = plt.bar(range(10), predictions_array, color ="#777777")
-        plt.ylim([0, 1])
-        predicted_label = np.argmax(predictions_array)
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color ="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
 
-        thisplot[predicted_label].set_color('red')
-        thisplot[true_label].set_color('blue')
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
 
     i = 0
     plt.figure(figsize=(6,3))
@@ -116,3 +108,31 @@ def plot_image(i, predictions_array, train_label, img):
     plt.subplot(1,2,2)
     plot_value_array(i, predictions[i], test_labels)
     plt.show()
+
+    num_rows = 5
+    num_cols = 3
+
+    num_images = num_rows*num_cols
+
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2*num_cols, 2*i+1)
+        plot_image(i, predictions[i], test_labels, test_image)
+        plt.subplot(num_rows, 2*num_cols, 2*i+2)
+        plot_value_array(i, predictions[i], test_labels)
+        plt.tight_layout()
+        plt.show()
+
+        img = test_image[1]
+        print(img.shape)
+
+        img = (np.expand_dims(img,0))
+        print(img.shape)
+
+        predictions_single = probability_model.predict(img)
+        print(predictions_single)
+
+    plot_value_array(1, predictions_single[0], test_labels)
+    _ = plt.xticks(range(10), class_names, rotation=45)
+
+    np.argmax(predictions_single[0])
